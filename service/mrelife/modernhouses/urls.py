b5677cc08ed85969849cccd0c350c187128083fb
vudung92 <dung.vuthi@mor.com.vn>
2018-11-05 15:56:37 +0700

    update code  RANWAY_RELIFE-50 View Modern house list

--- service/mrelife/modernhouses/urls.py
+++ service/mrelife/modernhouses/urls.py
@@ -11,11 +11,7 @@ router = routers.SimpleRouter()
 router.register(r'', views.ModernHouseViewSet)
 
 urlpatterns = [
-    #path('get/', csrf_exempt(views.OutletStoreList.as_view())),
-    #path('create/', csrf_exempt(views.OutletStoreCreate.as_view())),
-    #path('update/<int:pk>/', csrf_exempt(views.OutletStoreUpdate.as_view())),
-    path('', include(router.urls)),
-    #path('delete/<int:pk>/', csrf_exempt(views.OutletStoreDelete.as_view())),
+    path('', include(router.urls))
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
