43b2f125ee5c81e56fba1f743112947a24224f0c
Trinh Anh Chien <chien.trinhanh@mor.com.vn>
2018-11-06 10:12:00 +0700

    update save raleted data store

--- service/mrelife/outletstores/serializers.py
+++ service/mrelife/outletstores/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from mrelife.outletstores.models import OutletStore, Tag
+from mrelife.outletstores.models import OutletStore, Tag, OutletStoreTag
 
 class OutletStoreSerializer(serializers.ModelSerializer):
 
     thumbnail = serializers.ImageField(max_length=None, use_url=True)
-
+    
     class Meta:
         model = OutletStore
         fields = ('id', 'title', 'content', 'category', 'thumbnail', 'latitude', 'longitude', 'address',
@@ -27,10 +27,23 @@ class OutletStoreSerializer(serializers.ModelSerializer):
             updated=validated_data['updated']
         )
         outletstore = OutletStore.objects.create(**validated_data)
+        outlet_store_tag = OutletStoreTag.objects.create(
+            outlet_store=outletstore,
+            tag=tag,
+            is_active=True,
+            created=validated_data['created'],
+            updated=validated_data['updated']
+        )
         return outletstore
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ('name')
+        fields = ('name', )
+class OutletStoreTagSerializer(serializers.ModelSerializer):
+    outlet_store = OutletStoreSerializer(many=True)
+    tag = TagSerializer(many=True)
+    class Meta:
+        model = OutletStoreTag
+        fields = ('outlet_store', 'tag', )
 
