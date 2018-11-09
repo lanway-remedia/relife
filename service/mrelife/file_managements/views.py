77c6f06d5d80e0315b5634e6c358d367e01fc139
tu.nguyen@mor.com.vn <bin.ngoctu@gmail.com>
2018-11-09 10:51:33 +0700

    update return parse on File management

--- service/mrelife/file_managements/views.py
+++ service/mrelife/file_managements/views.py
@@ -2,7 +2,6 @@ from django.core.files.storage import default_storage
 
 from rest_framework import status
 from rest_framework.decorators import permission_classes
-from rest_framework.exceptions import ParseError
 from rest_framework.parsers import FileUploadParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -15,9 +14,19 @@ class MyUploadView(APIView):
 
     def post(self, request, format=None):
         if 'file' not in request.data:
-            raise ParseError("Empty content")
+            return Response({
+                'status': False,
+                'messageCode': 'FM002',
+                'messageParams': {},
+                'data': {}
+            }, status=status.HTTP_400_BAD_REQUEST)
 
         f = request.data['file']
         file = default_storage.save(f)
 
-        return Response({"url": file}, status=status.HTTP_201_CREATED)
+        return Response({
+            'status': True,
+            'messageCode': 'FM001',
+            'messageParams': {},
+            'data': {"url": file}
+        }, status=status.HTTP_201_CREATED)
