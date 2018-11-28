240759d26e083239b16a3f12924635415b55369d
namnh-at-204653788956 <namnh@evolableasia.vn>
2018-11-28 17:20:45 +0700

    FE: Add user search conditions

--- relife_lanway/app/services/wrapper/UsersService.js
+++ relife_lanway/app/services/wrapper/UsersService.js
@@ -7,7 +7,6 @@ import apiClient from '../../shared/apiClient'
 
 export default class UsersService {
   listUser(data) {
-    console.log(data)
     return apiClient.get(
       `/users/v1/users/?limit=${data.limit}&offset=${data.offset}${
         data.name ? `&name=${data.name}` : ''
