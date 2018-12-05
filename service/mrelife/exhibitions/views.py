dadf55f8195741be4f6594fb3be1ff283f891408
vudung92 <dung.vuthi@mor.com.vn>
2018-12-05 14:44:56 +0700

    update to exhibition

--- service/mrelife/exhibitions/views.py
+++ service/mrelife/exhibitions/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from django.conf import settings
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.db import transaction
 from rest_framework import status, viewsets
 from rest_framework.authentication import (BasicAuthentication,
                                            SessionAuthentication)
@@ -11,6 +12,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
 
 from mrelife.commons.common_fnc import CommonFuntion
 from mrelife.events.models import EventExhibition
@@ -29,7 +31,7 @@ class EhibitionViewSet(viewsets.ModelViewSet):
     queryset = Exhibition.objects.filter(is_active=settings.IS_ACTIVE).order_by('-updated')
     serializer_class = ExhibitionSerializer
     pagination_class = LimitOffsetPagination
-    permission_classes = (IsAuthenticated,)
+    #permission_classes = (IsAuthenticated,)
 
     def list(self, request, *args, **kwargs):
         self.queryset = Exhibition.objects.filter(is_active=settings.IS_ACTIVE).order_by("-updated")
@@ -44,6 +46,7 @@ class EhibitionViewSet(viewsets.ModelViewSet):
         except Exception as e:
             return Response(CommonFuntion.resultResponse(False, "", MessageCode.EX003.value, ""), status=status.HTTP_404_NOT_FOUND)
 
+    @transaction.atomic
     def create(self, request):
         try:
             self.parser_class = (FormParser, MultiPartParser)
@@ -63,8 +66,10 @@ class EhibitionViewSet(viewsets.ModelViewSet):
                 return Response(CommonFuntion.resultResponse(True, serializer.data, MessageCode.EX004.value, ""), status=status.HTTP_201_CREATED)
             return Response(CommonFuntion.resultResponse(False, "", MessageCode.EX005.value, serializer.errors), status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response(CommonFuntion.resultResponse(False, "", MessageCode.EX005.value, ""), status=status.HTTP_400_BAD_REQUEST)
-
+            transaction.set_rollback(True)
+            return Response(CommonFuntion.resultResponse(False, "", MessageCode.EX005.value, print(e)), status=status.HTTP_400_BAD_REQUEST)
+    
+    @transaction.atomic
     def update(self, request, pk=None):
         try:
             queryset = Exhibition.objects.all()
@@ -95,6 +100,7 @@ class EhibitionViewSet(viewsets.ModelViewSet):
                 return Response(CommonFuntion.resultResponse(True, serializer.data, MessageCode.EX006.value, ""), status=status.HTTP_200_OK)
             return Response(CommonFuntion.resultResponse(False, "", MessageCode.EX007.value, serializer.errors), status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
+            transaction.set_rollback(True)
             return Response(CommonFuntion.resultResponse(False, "", MessageCode.EX007.value, e), status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None):
