9b3e4d6078250a753cc6ff65f69d99e40924e8b1
vudung92 <dung.vuthi@mor.com.vn>
2018-12-06 11:00:39 +0700

    update to viiew exhibitiontag

--- service/mrelife/exhibitions/urls.py
+++ service/mrelife/exhibitions/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from mrelife.exhibitions import views
-from mrelife.exhibitions.exhibitiontags.views import EhibitionTagViewSet
+from mrelife.exhibitions.exhibitiontags.views import ExhibitionTagViewSet
 
 
 router = routers.SimpleRouter()
