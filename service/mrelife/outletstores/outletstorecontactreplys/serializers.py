5911b796301286f039b2eb9b20430f07a5ef4b1f
vudung92 <dung.vuthi@mor.com.vn>
2018-12-08 22:12:23 +0700

    update to store contact

--- service/mrelife/outletstores/outletstorecontactreplys/serializers.py
+++ service/mrelife/outletstores/outletstorecontactreplys/serializers.py
@@ -7,14 +7,20 @@ from rest_framework.validators import UniqueValidator
 from mrelife.locations.models import District
 from mrelife.locations.serializers import DistrictSerializer
 from mrelife.outletstores.models import OutletStoreContact, OutletStoreContactReply
-from mrelife.outletstores.ouletstorecontacts.serializers import OutletStoreContactSerializer
+#from mrelife.outletstores.ouletstorecontacts.serializers import OutletStoreContactSerializer
 from mrelife.users.models import User
 from mrelife.users.serializers import UserSerializer
+
+class OutletStoreVContactSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OutletStoreContact
+        fields = '__all__'
 
 
 class OutletStoreContactReplySerializer(serializers.ModelSerializer):
     outlet_store_contact_id = serializers.PrimaryKeyRelatedField(queryset=OutletStoreContact.objects.filter(is_active=1), write_only=True)
-    outlet_store_contact = OutletStoreContactSerializer(read_only=True,many=False)
+    outlet_store_contact = OutletStoreVContactSerializer(read_only=True,many=False)
     user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(is_active=1), write_only=True)
     user = UserSerializer(read_only=True)
     comment = serializers.CharField(max_length=255)
