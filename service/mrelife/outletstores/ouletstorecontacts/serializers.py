60701d8bb851adee6d1dbd6b1d1afdbbf02994bb
vudung92 <dung.vuthi@mor.com.vn>
2018-12-08 22:39:05 +0700

    update to store contact

--- service/mrelife/outletstores/ouletstorecontacts/serializers.py
+++ service/mrelife/outletstores/ouletstorecontacts/serializers.py
@@ -6,33 +6,35 @@ from rest_framework.validators import UniqueValidator
 
 from mrelife.locations.models import District
 from mrelife.locations.serializers import DistrictSerializer
-from mrelife.outletstores.models import (OutletStore, OutletStoreContact,
-                                         OutletStoreContactReply)
-from mrelife.outletstores.outletstorecontactreplys.serializers import OutletStoreContactReplySerializer
+from mrelife.outletstores.models import OutletStore, OutletStoreContact, OutletStoreContactReply
 from mrelife.outletstores.serializers import OutletStoreSerializer
 from mrelife.users.models import User
 from mrelife.users.serializers import UserSerializer
 
-class OutletStoreContacVSerializer(serializers.ModelSerializer):
-    outlet_store = OutletStoreSerializer(read_only=True)
-    create_user = UserSerializer(read_only=True)
+
+class OutletStoreContactVReplySerializer(serializers.ModelSerializer):
+
+    user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(is_active=1), write_only=True)
+    user = UserSerializer(read_only=True)
     comment = serializers.CharField(max_length=255)
     is_active = serializers.BooleanField(default=True)
 
     class Meta:
-        model = OutletStoreContact
-        fields = ('id', 'comment', 'create_user', 'outlet_store', 'is_active')
+        model = OutletStoreContactReply
+        fields = ('user_id', 'user', 'comment', 'is_active')
 
 
 class OutletStoreContactSerializer(serializers.ModelSerializer):
-    outlet_store_id = serializers.PrimaryKeyRelatedField(queryset=OutletStore.objects.filter(is_active=1), write_only=True)
+    outlet_store_id = serializers.PrimaryKeyRelatedField(
+        queryset=OutletStore.objects.filter(is_active=1), write_only=True)
     create_user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(is_active=1), write_only=True)
     outlet_store = OutletStoreSerializer(read_only=True)
     create_user = UserSerializer(read_only=True)
     comment = serializers.CharField(max_length=255)
-    outlet_store_contact_relpy = OutletStoreContactReplySerializer(many=True, read_only=True)
+    outlet_store_contact_relpy = OutletStoreContactVReplySerializer(many=True, read_only=True)
     is_active = serializers.BooleanField(default=True)
 
     class Meta:
         model = OutletStoreContact
-        fields = ('id', 'comment', 'create_user', 'outlet_store_id', 'create_user_id', 'outlet_store', 'outlet_store_contact_relpy', 'is_active')
+        fields = ('id', 'comment', 'create_user', 'outlet_store_id', 'create_user_id',
+                  'outlet_store', 'outlet_store_contact_relpy', 'is_active')
--- service/mrelife/outletstores/outletstorecontactreplys/serializers.py
+++ service/mrelife/outletstores/outletstorecontactreplys/serializers.py
@@ -6,11 +6,22 @@ from rest_framework.validators import UniqueValidator
 
 from mrelife.locations.models import District
 from mrelife.locations.serializers import DistrictSerializer
-from mrelife.outletstores.models import (OutletStoreContact,
-                                         OutletStoreContactReply)
-from mrelife.outletstores.ouletstorecontacts.serializers import OutletStoreContacVSerializer
+from mrelife.outletstores.models import OutletStoreContact, OutletStoreContactReply
+from mrelife.outletstores.serializers import OutletStoreSerializer
 from mrelife.users.models import User
 from mrelife.users.serializers import UserSerializer
+
+
+class OutletStoreContacVSerializer(serializers.ModelSerializer):
+    outlet_store = OutletStoreSerializer(read_only=True)
+    create_user = UserSerializer(read_only=True)
+    comment = serializers.CharField(max_length=255)
+    is_active = serializers.BooleanField(default=True)
+
+    class Meta:
+        model = OutletStoreContact
+        fields = ('id', 'comment', 'create_user', 'outlet_store', 'is_active')
+
 
 class OutletStoreContactReplySerializer(serializers.ModelSerializer):
     outlet_store_contact_id = serializers.PrimaryKeyRelatedField(queryset=OutletStoreContact.objects.filter(is_active=1), write_only=True)
