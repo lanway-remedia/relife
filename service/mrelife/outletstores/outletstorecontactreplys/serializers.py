cf3ae8c42b2cd6f38edb91da7933c5f7f9e83a9c
vudung92 <dung.vuthi@mor.com.vn>
2018-12-08 22:31:55 +0700

    update to store contact

--- service/mrelife/outletstores/outletstorecontactreplys/serializers.py
+++ service/mrelife/outletstores/outletstorecontactreplys/serializers.py
@@ -6,21 +6,15 @@ from rest_framework.validators import UniqueValidator
 
 from mrelife.locations.models import District
 from mrelife.locations.serializers import DistrictSerializer
-from mrelife.outletstores.models import OutletStoreContact, OutletStoreContactReply
-#from mrelife.outletstores.ouletstorecontacts.serializers import OutletStoreContactSerializer
+from mrelife.outletstores.models import (OutletStoreContact,
+                                         OutletStoreContactReply)
+from mrelife.outletstores.ouletstorecontacts.serializers import OutletStoreContacVSerializer
 from mrelife.users.models import User
 from mrelife.users.serializers import UserSerializer
 
-class OutletStoreVContactSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = OutletStoreContact
-        fields = '__all__'
-
-
 class OutletStoreContactReplySerializer(serializers.ModelSerializer):
     outlet_store_contact_id = serializers.PrimaryKeyRelatedField(queryset=OutletStoreContact.objects.filter(is_active=1), write_only=True)
-    outlet_store_contact = OutletStoreVContactSerializer(read_only=True,many=False)
+    outlet_store_contact = OutletStoreContacVSerializer(read_only=True, many=False)
     user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(is_active=1), write_only=True)
     user = UserSerializer(read_only=True)
     comment = serializers.CharField(max_length=255)
@@ -28,4 +22,4 @@ class OutletStoreContactReplySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = OutletStoreContactReply
-        fields = ('outlet_store_contact_id','outlet_store_contact', 'user_id', 'user', 'comment', 'is_active')
+        fields = ('outlet_store_contact_id', 'outlet_store_contact', 'user_id', 'user', 'comment', 'is_active')
