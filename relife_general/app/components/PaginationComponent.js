8b8811ab2e696014bce9989b011f94a3487f6a37
hanhtd <hanh.tongduc@mor.com.vn>
2018-12-21 15:58:56 +0700

    RL General: get data example house

--- relife_general/app/components/PaginationComponent.js
+++ relife_general/app/components/PaginationComponent.js
@@ -50,29 +50,6 @@ class PaginationComponent extends Component {
     let pagesCount = count == 0 ? 1 : Math.ceil(count / limit)
     return (
       <div className="toolbar mb-5">
-        <div className="total">
-          <span>
-            {I18nUtils.formatMessage(
-              { id: 'toolbar-totalRecords' },
-              { limit: limit, total: count }
-            )}
-          </span>
-        </div>
-        <div className="limiter">
-          <Label for="limit">{I18nUtils.t('toolbar-limit')}</Label>
-          <Input
-            type="select"
-            name="limit"
-            id="limit"
-            value={limit}
-            onChange={this.onPerpageChange}
-          >
-            <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-          </Input>
-        </div>
         {count > 0 && (
           <UltimatePagination
             currentPage={page}
