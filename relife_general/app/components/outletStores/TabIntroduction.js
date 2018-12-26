db02c77560fcb2c89dce0c03c02cbb0c5ae5c50e
hanhtd <hanh.tongduc@mor.com.vn>
2018-12-26 11:19:42 +0700

    RL General: outlet store view tab

--- relife_general/app/components/outletStores/TabIntroduction.js
+++ relife_general/app/components/outletStores/TabIntroduction.js
@@ -1,23 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { withRouter, Link } from 'react-router-dom'
-import OutletStoreActions from '../../redux/wrapper/OutletStoresRedux'
-import { DefaultValue } from '../../constants'
+import { withRouter } from 'react-router-dom'
 import I18nUtils from '../../utils/I18nUtils'
 
 class TabIntroduction extends React.Component {
   render () {
+    let outletStore = this.props.outletStore
     return (
       <div className="content">
         <div className="builder-detail clearfix">
           <h2 className="detail-subtitle">
-            会社紹介
+            {I18nUtils.t('store-intro-title')}
           </h2>
+          <div className="company-intro">
+            {outletStore.content}
+          </div>
+          <h2 className="detail-subtitle">
+          {I18nUtils.t('store-data-title')}
+          </h2>
+          <div className="company-data">
+            <table>
+              <tbody>
+                <tr>
+                  <th>会社名</th>
+                  <td>{outletStore.title}</td>
+                </tr>
+                <tr>
+                  <th>所在地</th>
+                  <td>{outletStore.district && outletStore.district.name} {outletStore.district && outletStore.district.city ? outletStore.district.city.name : ''}</td>
+                </tr>
+                
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     )
   }
+}
+
+TabIntroduction.propTypes = {
+  outletStore: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.object,
+  ]),
 }
 
 export default connect()(withRouter(TabIntroduction))
