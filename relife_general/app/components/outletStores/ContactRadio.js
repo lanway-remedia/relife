c08e2aaef494e4b0c4985ec5ba8df34b7df207f5
hanhtd <hanh.tongduc@mor.com.vn>
2018-12-27 17:48:27 +0700

    RL General: contact form store

---
+++ relife_general/app/components/outletStores/ContactRadio.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { PropTypes } from 'prop-types'
+
+class ContactRadio extends 
--- relife_general/app/containers/outletStores/OutletStoresViewPage.js
+++ relife_general/app/containers/outletStores/OutletStoresViewPage.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { withRouter, Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import OutletStoreActions from '../../redux/wrapper/OutletStoresRedux'
-import I18nUtils from '../../utils/I18nUtils'
 
 import Tabs from './../../components/outletStores/Tabs'
 
