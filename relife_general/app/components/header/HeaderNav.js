f6048408c757e0e24c7f68ff3737d32e207f05cc
hanhtd <hanh.tongduc@mor.com.vn>
2019-01-02 14:01:46 +0700

    RL general: nav search in header

--- relife_general/app/components/header/HeaderNav.js
+++ relife_general/app/components/header/HeaderNav.js
@@ -2,48 +2,55 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
 import I18nUtils from '../../utils/I18nUtils'
-import { Form } from'reactstrap'
 
+import HeaderSubMenu from './HeaderSubMenu'
 class HeaderNav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showMenu: false
+    }
+  }
+
+  handleOnMouseEnter = () => {
+    this.setState({
+      showMenu: true
+    })
+    document.getElementById('gnav').style.zIndex = 10000
+  }
+
+  handleOnMouseLeave = () => {
+    this.setState({
+      showMenu: false
+    })
+    document.getElementById('gnav').style.zIndex = 0
+  }
+
   render() {
+    let { showMenu } = this.state
     return (
       <nav id="gnav">
         <ul className="gnav">
-          <li className="menu__mega">
+          <li className="menu__mega"
+            onMouseEnter={this.handleOnMouseEnter}
+            onMouseLeave={this.handleOnMouseLeave}
+          >
             <Link to="/builder">{I18nUtils.t('builder-title')}</Link>
-            <div className="menu__second-level clearfix">
-              <div className="mega-menu-inner-left">
-                <div className="top-map-list">
-                  <div className="top-map-list-once clearfix">
-                    <div className="top-map-list-area">北海道・東北</div>
-                    <ul>
-                      <li>
-                        <Link to="">aaa</Link>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-
-              <div className="mega-menu-inner-right">
-                <div className="mega-menu-form">
-                  <div className="mega-menu-inner-title">キーワードで検索</div>
-                </div>
-                <Form>
-
-                </Form>
-              </div>
-            </div>
+              {showMenu &&
+                <HeaderSubMenu />
+              }
           </li>
           <li>
             <Link to="/example">{I18nUtils.t('example-house-title')}</Link>
           </li>
           <li>
-            <Link to="/example">{I18nUtils.t('re-style-title')}</Link>
+            <Link to="/style">{I18nUtils.t('re-style-title')}</Link>
           </li>
         </ul>
       </nav>
     )
   }
 }
-export default connect()(withRouter(HeaderNav))
+
+export default connect(
+)(withRouter(HeaderNav))
