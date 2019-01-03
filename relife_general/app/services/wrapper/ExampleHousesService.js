2f6472b9b48a5fe4350197a32c9ad6628fdb04ff
hanhtd <hanh.tongduc@mor.com.vn>
2019-01-03 17:45:03 +0700

    RL General: review code search

--- relife_general/app/services/wrapper/ExampleHousesService.js
+++ relife_general/app/services/wrapper/ExampleHousesService.js
@@ -2,6 +2,7 @@ import apiClient from '../../shared/apiClient'
 export default class ExampleHousesService {
   listExampleHouses(data) {
     let search = new URLSearchParams(data)
+    console.log(search.toString())
     return apiClient.get(
       `/examplehouses/v1/?${search.toString()}`
     )
