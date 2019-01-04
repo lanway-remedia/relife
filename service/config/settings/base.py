032401b5ae555f6f3a5a2650fc7eaf4b2ea161d9
vudung92 <dung.vuthi@mor.com.vn>
2019-01-04 09:51:19 +0700

    update to store contact

--- service/config/settings/base.py
+++ service/config/settings/base.py
@@ -68,6 +68,7 @@ THIRD_PARTY_APPS = [
     'rest_framework_jwt',
     'rest_framework.authtoken',
     'rest_framework_swagger',
+    'django_inlinecss',
     # Filter on URL
     'url_filter',
     'corsheaders',
