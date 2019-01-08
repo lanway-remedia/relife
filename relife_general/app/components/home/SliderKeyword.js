279ba7e66aa3798a0d74831ebe10d842d87b9975
hanhtd <hanh.tongduc@mor.com.vn>
2019-01-08 15:19:16 +0700

    RL General: data slider homepage

--- relife_general/app/components/home/SliderKeyword.js
+++ relife_general/app/components/home/SliderKeyword.js
@@ -4,6 +4,7 @@
  */
 import React from 'react'
 import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import {Container, Row, Col} from 'reactstrap'
 import { withRouter, Link } from 'react-router-dom'
 import Slider from 'react-slick'
@@ -11,10 +12,36 @@ import Slider from 'react-slick'
 import slider01 from '../../images/slider-01.jpg'
 import slider02 from '../../images/slider-02.jpg'
 import slider03 from '../../images/slider-03.jpg'
-import slider04 from '../../images/slider-04.png'
+import slider04 from '../../images/slider-04.jpg'
 import slider05 from '../../images/slider-05.jpg'
+import AttributeActions from '../../redux/wrapper/AttributesRedux'
+import I18nUtils from '../../utils/I18nUtils'
 
 class SliderKeyword extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      listPrice: [],
+    }
+  }
+  componentDidMount() {
+    let data = {
+      limit: '',
+      offset: '' 
+    }
+    this.props.attributePriceListRequest(data)
+  }
+  componentWillReceiveProps(nextProps) {
+    if (this.props.data != nextProps.data) {
+      let response = nextProps.data
+      //list price range
+      if (response.isGetListPrice) {
+        this.setState({
+          listPrice: response.data
+        })
+      }
+    }
+  }
   render() {
     const settings = {
       infinite: true,
@@ -30,107 +57,49 @@ class SliderKeyword extends React.Component {
         }
       ]
     }
+    let { listPrice } = this.state
+    let arrImgPrice = [slider01, slider02, slider03, slider04, slider05]
 
     return (
       <Container fluid className="top-result-search padding-0">
         <Row className="top-result-search-inner">
           <Col md="12" xs="12">
             <h4 className="top-result-search-title">
-              予算から建築実績を探す
+              {I18nUtils.t('search-from-price')}
             </h4>
           </Col>
           <div className="slider-search-wrap" id="my-slider2">
             <Slider {...settings}>
-              <div className="slider-item">
-                <Link to="">
-                  <img src={slider01} />
+              {listPrice.map((val, key) => (
+                <div className="slider-item" key={key}>
+                <Link to={`example/?price_range__in=${val.id}`}>
+                  <img src={arrImgPrice[key % 5]} />
                   <div className="slider-title">
-                    <h3>ローコスト</h3>
+                    <h3>{val.title}</h3>
                   </div>
                 </Link>
-              </div>
-              <div className="slider-item">
-                <Link to="">
-                  <img src={slider02} />
-                  <div className="slider-title">
-                    <h3>1000万円台</h3>
-                  </div>
-                </Link>
-              </div>
-              <div className="slider-item">
-                <Link to="">
-                  <img src={slider03} />
-                  <div className="slider-title">
-                    <h3>2000万円台</h3>
-                  </div>
-                </Link>
-              </div>
-              <div className="slider-item">
-                <Link to="">
-                  <img src={slider04} />
-                  <div className="slider-title">
-                    <h3>3000万円台</h3>
-                  </div>
-                </Link>
-              </div>
-              <div className="slider-item">
-                <Link to="">
-                  <img src={slider05} />
-                  <div className="slider-title">
-                    <h3>3000万円台</h3>
-                  </div>
-                </Link>
-              </div>
+                </div>
+              ))}
             </Slider>
           </div>
 
           <Col md="12" xs="12">
             <h4 className="top-result-search-title">
-              こだわりから建築実績を探す
+              {I18nUtils.t('search-from-performance')}
             </h4>
           </Col>
           <div className="slider-search-wrap" id="my-slider3">
           <Slider {...settings}>
-            <div className="slider-item">
-              <Link to="">
-                <img src={slider01} />
+            {listPrice.map((val, key) => (
+              <div className="slider-item" key={key}>
+              <Link to={`example/?price_range__in=${val.id}`}>
+                <img src={arrImgPrice[key % 5]} />
                 <div className="slider-title">
-                  <h3>狭小住宅</h3>
+                  <h3>{val.title}</h3>
                 </div>
               </Link>
-            </div>
-            <div className="slider-item">
-              <Link to="">
-                <img src={slider02} />
-                <div className="slider-title">
-                  <h3>二世帯住宅</h3>
-                </div>
-              </Link>
-            </div>
-            <div className="slider-item">
-              <Link to="">
-                <img src={slider03} />
-                <div className="slider-title">
-                  <h3>地下室</h3>
-                </div>
-              </Link>
-            </div>
-            <div className="slider-item">
-              <Link to="">
-                <img src={slider04} />
-                <div className="slider-title">
-                  <h3>平屋住宅</h3>
-                </div>
-              </Link>
-            </div>
-            <div className="slider-item">
-              <Link to="">
-                <img src={slider05} />
-                <div className="slider-title">
-                  <h3>平屋住宅</h3>
-                </div>
-              </Link>
-            </div>
+              </div>
+            ))}
           </Slider>
           </div>
         </Row>
@@ -138,4 +107,26 @@ class SliderKeyword extends React.Component {
     )
   }
 }
-export default connect()(withRouter(SliderKeyword))
+
+SliderKeyword.propTypes = {
+  processing: PropTypes.bool,
+  data: PropTypes.object,
+  attributePriceListRequest: PropTypes.func,
+}
+
+const mapStateToProps = state => {
+  return {
+    processing: state.attributes.processing,
+    data: state.attributes.data
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  attributePriceListRequest: data =>
+    dispatch(AttributeActions.attributePriceListRequest(data)),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(SliderKeyword))
