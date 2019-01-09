7d2920fb6663fd44baa50349f691edc541003644
hanhtd <hanh.tongduc@mor.com.vn>
2019-01-09 09:36:25 +0700

    RL General : header sp + slider respon

--- relife_general/app/components/outletStores/TabIntroduction.js
+++ relife_general/app/components/outletStores/TabIntroduction.js
@@ -8,6 +8,22 @@ import I18nUtils from '../../utils/I18nUtils'
 class TabIntroduction extends React.Component {
   render () {
     let outletStore = this.props.outletStore
+    const arrPrice = [
+      '下限なし',
+      '20万円以上',
+      '30万円以上',
+      '40万円以上',
+      '50万円以上',
+      '60万円以上',
+      '70万円以上',
+      '80万円以上',
+      '90万円以上',
+      '100万円以上',
+      '100万円以上',
+      '110万円以上',
+      '120万円以上',
+      '上限なし'
+    ]
     return (
       <Row className="tab-content-item">
         <Col xs="12" md="12" className="tab-intro">
@@ -23,14 +39,72 @@ class TabIntroduction extends React.Component {
           <div className="company-data">
             <Table>
               <tbody>
-                <tr>
-                  <th>会社名</th>
-                  <td>{outletStore.title}</td>
-                </tr>
-                <tr>
-                  <th>所在地</th>
-                  <td>{outletStore.address}</td>
-                </tr>
+                {outletStore.title && (
+                  <tr>
+                    <th>{I18nUtils.t('company-name')}</th>
+                    <td>{outletStore.title}</td>
+                  </tr>
+                )}
+                {outletStore.establish && (
+                  <tr>
+                    <th>{I18nUtils.t('company-establish')}</th>
+                    <td>{outletStore.establish}</td>
+                  </tr>
+                )}
+                {outletStore.address && (
+                  <tr>
+                    <th>{I18nUtils.t('company-address')}</th>
+                    <td>{outletStore.address}</td>
+                  </tr>
+                )}
+                {outletStore.charter_capital && (
+                  <tr>
+                    <th>{I18nUtils.t('company-charter-capital')}</th>
+                    <td>{outletStore.charter_capital}円</td>
+                  </tr>
+                )}
+                {outletStore.employee_total && (
+                  <tr>
+                    <th>{I18nUtils.t('company-employee-total')}</th>
+                    <td>{outletStore.employee_total}</td>
+                  </tr>
+                )}
+                {outletStore.qualification && (
+                  <tr>
+                    <th>{I18nUtils.t('company-qualification')}</th>
+                    <td>{outletStore.qualification}</td>
+                  </tr>
+                )}
+                {outletStore.permit_number && (
+                  <tr>
+                    <th>{I18nUtils.t('company-permit-number')}</th>
+                    <td>{outletStore.permit_number}</td>
+                  </tr>
+                )}
+                {outletStore.construction_area && (
+                  <tr>
+                    <th>{I18nUtils.t('company-construction-area')}</th>
+                    <td>{outletStore.construction_area}</td>
+                  </tr>
+                )}
+                {outletStore.construction_result && (
+                  <tr>
+                    <th>{I18nUtils.t('company-construction-result')}</th>
+                    <td>{outletStore.construction_result}</td>
+                  </tr>
+                )}
+                {outletStore.min_price && (
+                  <tr>
+                    <th>{I18nUtils.t('company-price')}</th>
+                    <td>{arrPrice[outletStore.min_price]}</td>
+                  </tr>
+                )}
+                {outletStore.time_serving && (
+                  <tr>
+                    <th>{I18nUtils.t('company-time-serving')}</th>
+                    <td>{outletStore.time_serving}</td>
+                  </tr>
+                )}
                 
               </tbody>
             </Table>
